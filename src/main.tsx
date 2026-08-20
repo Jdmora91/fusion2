4588fe6b1eb385966da6fa9573d4bdc11f4dde82
Jose Mora <j.donismora@gmail.com>
2026-08-20 13:44:35 -0600

    feat: refine gallery, sushi motion, map and footer

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import './styles/variables.css'
 import './styles/reset.css'
 import './styles/global.css'
 import './styles/animations.css'
+import './styles/map.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
